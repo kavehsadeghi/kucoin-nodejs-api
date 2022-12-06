534117a3aa8535d5659f5cc0c93e5d19b4999e2e
kaveh <kaveh.sadeghi.garmaroudi@gmail.com>
2022-12-06 16:49:37 +0330

    add example

--- test.js
+++ test.js
@@ -6,6 +6,45 @@ const config = {
     environment: 'live',
   };
   api.init(config);
+/*
+(async () => {
+    params = {
+        topic: "allTicker",
+        // symbols: "BTC,",
+    }
+    api.initSocket(params, (data) => {
+        console.log(data);
+    })
+})();
+
+(async () => {
+    const params = {
+        base: 'EUR'
+    };
+    console.log(await api.getFiatPrice(params));
+})();
+
+(async () => {
+    console.log(await api.getCurrencies());
+})();
+
+(async () => {
+    const data = await api.getCurrency('BTC');
+    console.log(data.data);
+})();
+
+(async () => {
+    console.log(await api.getAllTickers());
+})();
+
+(async () => {
+    console.log(await api.getTicker('BTC-USDT'));
+})();
+
+(async () => {
+    console.log(await api.getSymbols());
+})();
+
 (async () => {
     console.log(await api.balance());
 })();
@@ -21,3 +60,31 @@ const config = {
     }
     console.log(await api.getActualTradeFee(params));
 })();
+
+(async () => {
+    // market buy request
+    const params = {
+        clientOid: 'AF-1',
+        side: 'buy',
+        symbol: 'XRP-USDT',
+        type: 'market',
+        remark: 'test',
+        size: "35",
+        tradeType: "TRADE"
+    }
+    console.log(await api.placeOrder(params));
+})();
+
+(async () => {
+    // market sell request
+    const params = {
+        clientOid: 'AF-1',
+        side: 'sell',
+        symbol: 'XRP-USDT',
+        type: 'market',
+        remark: 'test',
+        size: "35",
+        tradeType: "TRADE"
+    }
+    console.log(await api.placeOrder(params));
+})();*/
